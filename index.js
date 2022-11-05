1fbac40d81572496ad5eec576915952d7b675cdc
Arif Jahan <arifjahan88@gmail.com>
2022-11-05 22:19:59 +0600

    after complete jwt

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+
+//token = 58b83570442ef3a8c21434b1bfd4368196d69c49a52394a72785a6496847bc7b7d7b80e1d2d080183a5fc7cceb661c2d6f5af471ca4cd22d3f647683f71b70dc
 
 app.use(cors());
 app.use(express.json());
@@ -23,6 +26,35 @@ async function run() {
   try {
     const serviceCollection = client.db("geniusCar").collection("Services");
     const orderCollection = client.db("geniusCar").collection("Orders");
+
+    function verifyjwt(req, res, next) {
+      const authheaders = req.headers.authorization;
+      if (!authheaders) {
+        res.status(401).send({ message: "Unauthorized Access" });
+      }
+      const token = authheaders.split(" ")[1];
+
+      jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        function (err, decoded) {
+          if (err) {
+            res.status(403).send({ Message: "Unauthorized Access" });
+          }
+          req.decoded = decoded;
+          next();
+        }
+      );
+    }
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1hr",
+      });
+      res.send({ token });
+    });
+
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
@@ -35,13 +67,17 @@ async function run() {
       const service = await serviceCollection.findOne(query);
       res.send(service);
     });
-    //Orders Api
-    app.post("/orders", async (req, res) => {
+    app.post("/orders", verifyjwt, async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
       res.send(result);
     });
-    app.get("/orders", async (req, res) => {
+    //Orders Api
+    app.get("/orders", verifyjwt, async (req, res) => {
+      const decoded = req.decoded;
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ Message: "Unothorized Access" });
+      }
       let query = {};
       if (req.query.email) {
         query = {
@@ -52,7 +88,7 @@ async function run() {
       const orders = await cursor.toArray();
       res.send(orders);
     });
-    app.patch("/orders/:id", async (req, res) => {
+    app.patch("/orders/:id", verifyjwt, async (req, res) => {
       const id = req.params.id;
       const status = req.body.status;
       const query = { _id: ObjectId(id) };
@@ -64,7 +100,7 @@ async function run() {
       const result = await orderCollection.updateOne(query, updateDoc);
       res.send(result);
     });
-    app.delete("/orders/:id", async (req, res) => {
+    app.delete("/orders/:id", verifyjwt, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await orderCollection.deleteOne(query);
